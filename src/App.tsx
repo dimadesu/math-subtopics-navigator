bd5630d00811ccbeebed3120e33b19abca9a946d
Dmitry Antonov <antonov.dmitriy@gmail.com>
2017-11-28 21:33:02 +1300

    Add basic html and CSS class names

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,48 @@
 import * as React from 'react';
 import './App.css';
 
-const logo = require('./logo.svg');
-
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="app">
+        <div className="header">
+          <div className="header-group">
+            <div className="menu">
+              <img src="/assets/menu.svg" alt="Menu"/>
+            </div>
+            <div className="topic">Geometry</div>
+            <div className="avatar">
+              <img src="/assets/avatar.png" alt="Avatar"/>
+            </div>
+          </div>
+          <div className="navigation">
+            <div className="navigation__item">1</div>
+            <div className="navigation__item">2</div>
+            <div className="navigation__item">3</div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <div className="subtopics">
+          <div className="subtopic">
+            <div className="subtopic__title">
+              3. Similar Triangles
+            </div>
+            <div className="subtopic__completed--completed">
+              <img src="/assets/tick-big.svg" alt="Completed"/>
+            </div>
+            <button type="button">Let's Go</button>
+          </div>
+        </div>
+        <div className="tab-bar">
+          <div className="tab-bar__item--active">
+            <img src="/assets/tick.svg" alt="Tick"/>
+          </div>
+          <div className="tab-bar__item">
+            <img src="/assets/piechart.svg" alt="Pie chart"/>
+          </div>
+          <div className="tab-bar__item">
+            <img src="/assets/linegraph.svg" alt="Line graph"/>
+          </div>
+        </div>
       </div>
     );
   }
