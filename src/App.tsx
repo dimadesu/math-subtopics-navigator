8ba788f62c68453ed5fee25d9983d1220b3333bd
Dmitry Antonov <antonov.dmitriy@gmail.com>
2017-11-29 09:04:34 +1300

    Add navigation arrows

--- src/App.tsx
+++ src/App.tsx
@@ -16,28 +16,36 @@ class App extends React.Component {
             </div>
           </div>
           <div className="navigation">
-            <div className="navigation__item navigation__item--completed">
-              <a href="#" className="navigation__item__link">1</a>
+            <div className="navigation__arrow">
+              <img src="/assets/left.svg"/>
             </div>
-            <div className="navigation__item-connection"/>
-            <div className="navigation__item navigation__item--completed">
-              <a href="#" className="navigation__item__link">2</a>
+            <div className="navigation__items">
+              <div className="navigation__item navigation__item--completed">
+                <a href="#" className="navigation__item__link">1</a>
+              </div>
+              <div className="navigation__item-connection"/>
+              <div className="navigation__item navigation__item--completed">
+                <a href="#" className="navigation__item__link">2</a>
+              </div>
+              <div className="navigation__item-connection"/>
+              <div className="navigation__item navigation__item--completed navigation__item--active">
+                <a href="#" className="navigation__item__link">3</a>
+              </div>
+              <div className="navigation__item-connection"/>
+              <div className="navigation__item">
+                <a href="#" className="navigation__item__link">4</a>
+              </div>
+              <div className="navigation__item-connection"/>
+              <div className="navigation__item">
+                <a href="#" className="navigation__item__link">5</a>
+              </div>
+              <div className="navigation__item-connection"/>
+              <div className="navigation__item">
+                <a href="#" className="navigation__item__link">6</a>
+              </div>
             </div>
-            <div className="navigation__item-connection"/>
-            <div className="navigation__item navigation__item--completed navigation__item--active">
-              <a href="#" className="navigation__item__link">3</a>
-            </div>
-            <div className="navigation__item-connection"/>
-            <div className="navigation__item">
-              <a href="#" className="navigation__item__link">4</a>
-            </div>
-            <div className="navigation__item-connection"/>
-            <div className="navigation__item">
-              <a href="#" className="navigation__item__link">5</a>
-            </div>
-            <div className="navigation__item-connection"/>
-            <div className="navigation__item">
-              <a href="#" className="navigation__item__link">6</a>
+            <div className="navigation__arrow">
+              <img src="/assets/right.svg"/>
             </div>
           </div>
         </div>
